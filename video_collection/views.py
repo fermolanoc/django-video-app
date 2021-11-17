a2034413bf8d2d2239cd10c6b28a8e74b1fddb72
Fernando Molano <fm87usa@gmail.com>
2021-11-16 23:51:47 -0500

    add validation to form fields and show error messages for incomplete forms

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import VideoForm
 
 
@@ -8,5 +9,15 @@ def home(request):
 
 
 def add(request):
+
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save()
+            messages.info(request, 'New video saved!')
+            # todo show successful message or redirect to list of videos
+        else:
+            messages.info(request, 'Please check the data entered')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
