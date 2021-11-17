b7b5f0dcbdd4192f4cd94b5a699db6a8dfa1f249
Fernando Molano <fm87usa@gmail.com>
2021-11-17 17:53:30 -0500

    add video_id field to model, extract video id from url, improve messages shown when adding new video

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from .forms import VideoForm
 from .models import Video
 
@@ -14,12 +16,18 @@ def add(request):
     if request.method == 'POST':
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save()
-            messages.info(request, 'New video saved!')
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                # messages.info(request, 'New video saved!')
             # todo show successful message or redirect to list of videos
-        else:
-            messages.info(request, 'Please check the data entered')
-            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video')
+        
+        messages.info(request, 'Please check the data entered')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
 
