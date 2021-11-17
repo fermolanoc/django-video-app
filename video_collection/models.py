198e31b41b904a9fd05715e3a82561d2bee9a29b
Fernando Molano <fm87usa@gmail.com>
2021-11-16 22:14:07 -0500

    create model for Video

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True)
