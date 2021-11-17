9290b6630d109c08d159d4a659686e2e665c3204
Fernando Molano <fm87usa@gmail.com>
2021-11-16 23:32:29 -0500

    Create new view and url to add video. Add styles to form

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .forms import VideoForm
 
 
 def home(request):
     app_name = 'Coding Tutorials'
     return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+
+def add(request):
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
