e4f80e1ff38743f197be145e3dd61b0954283a8f
Fernando Molano <fm87usa@gmail.com>
2021-11-16 23:12:53 -0500

    Create folder structure, create home view with its html initial code and basic styles. Connect views with urls

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    app_name = 'Coding Tutorials'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
